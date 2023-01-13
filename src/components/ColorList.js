2b3019c14de98f9c679761deea36f7042a1edb61
prince475 <danielmwai475@gmail.com>
2023-01-13 11:50:30 +0300

    More on props and key prop

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -24,3 +24,66 @@ function ColorList() {
 }
 
 export default ColorList;
+
+//using .map method to transform our array into jsx
+// function ColorList() {
+//   const colors = [
+//     "firebrick",
+//     "rebeccapurple",
+//     "salmon",
+//     "darkslategray",
+//     "hotpink",
+//   ];
+
+//   const colorElements = colors.map((color) => {
+//     return <li style={{ color: color }}>{color}</li>;
+//   });
+//   return (
+//     <div>
+//       <h1>Top 5 CSS Colors</h1>
+//       <ol>
+//         {/* display the array of <li> elements here! */}
+//         {colorElements}
+//       </ol>
+//     </div>
+//   );
+// }
+
+// const colorElements = colors.map((color) => {
+  // return (
+    // <li key={color} style={{ color: color }}>
+    //   {/* {color} */}
+    // {/* </li> */}
+  // );
+// });
+// }
+
+//The key Prop this is to remove the wrning; each child in a list should have a unique "key" prop
+//checking the render method of ColorList
+// ColorItem component
+// function ColorItem(props) {
+//   return <li style={{ color: props.color }}>{props.color}</li>;
+// }
+
+// // ColorList component
+// function ColorList() {
+//   const colors = [
+//     "firebrick",
+//     "rebeccapurple",
+//     "salmon",
+//     "darkslategray",
+//     "hotpink",
+//   ];
+
+//   const colorElements = colors.map((color) => {
+//     return <ColorItem key={color} color={color} />;
+//   });
+//   // etc
+// }
+
+
+
+// export default ColorList;
+
+
+
